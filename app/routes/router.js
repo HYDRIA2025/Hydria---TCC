42fde035ebc0afd3fe4fff6fa3237e11665c7d9c
Britozx7 <166768537+Britozx7@users.noreply.github.com>
2025-07-05 17:38:55 -0300

    Co-authored-by: SophiaRibeiroGomesDosSantos <SophiaRibeiroGomesDosSantos@users.noreply.github.com>

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
 
 router.get('/', function(req, res) {
     res.render('pages/index')
 })
+
+router.get('/login', function(req, res) {
+    res.render('pages/login')
+})
+
+
+
 
 module.exports = router;
